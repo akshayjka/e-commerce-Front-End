d332a994f961bf48d87cb9360dbc682dc705d85d
akshay <akshayjai19001900@gmail.com>
2024-10-03 08:04:15 +0530

    fix issue

--- ecommerce-fr/src/app/app.module.ts
+++ ecommerce-fr/src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { InputComponent } from './shared/input/input.component';
 import { RegisterScreenComponent } from './register/register-screen/register-screen.component';
+import { InputComponent } from './shared/input/input.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { RegisterScreenComponent } from './register/register-screen/register-scr
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
